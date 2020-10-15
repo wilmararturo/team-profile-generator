cb9015918908d85bb76560042f52fd3901ab65b2
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-15 00:59:19 -0400

    fixes to questionaire

--- app.js
+++ app.js
@@ -9,17 +9,12 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 
-
+const responses = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 const roleChoices = [
-
-    {
-        name: "Employee",
-        value: "employee",
-        short: "employee",
-    },
     {
         name: "Engineer",
         value: "engineer",
@@ -65,34 +60,74 @@ const questions = [
         choices: roleChoices,
 
     },
-]
-
-const engineerQuestions = [
     {
         type: "input",
         name: "engineerGithub",
-        message: "GitHub Username:"
-    }
-]
-
-
-const managerQuestions = [
+        message: "GitHub Username:",
+        when: (answers) => answers.employeeRole === "engineer",
+    },
     {
         type: "input",
         name: "managerOffice",
-        message: "Office Number:"
-    }
-]
-
-
-const internQuestions = [
+        message: "Office Number:",
+        when: (answers) => answers.employeeRole === "manager",
+    },
     {
         type: "input",
         name: "internSchool",
-        message: "School:"
+        message: "School:",
+        when: (answers) => answers.employeeRole === "intern",
+    },
+    {
+        type: "confirm",
+        name: "addAnother",
+        message: "Add another employee",
+        default: true,
     }
+
 ]
 
+
+function getEmployees() {
+    inquirer.prompt(questions)
+        .then((answers) => {
+            switch (answers.employeeRole) {
+                case "intern":
+                    const intern = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.internSchool)
+                    responses.push(intern);
+                    console.log(responses);
+
+                    break;
+
+                case "engineer":
+                    const engineer = new Engineer(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.engineerGithub)
+                    responses.push(engineer);
+                    console.log(responses);
+
+
+                    break;
+                case "manager":
+                    const manager = new Manager(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.managerOffice)
+                    responses.push(manager);
+                    console.log(responses);
+
+                    break;
+
+                // default:
+                //     const employee = new Employee(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.managerOffice)
+                //     responses.push(employee);
+                //     console.log(responses);
+                //     break;
+            }
+            if (answers.addAnother) {
+                getEmployees();
+            } else {
+                const htmlOut = render(responses);
+                console.log(htmlOut);
+            }
+        });
+
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
@@ -112,3 +147,5 @@ const internQuestions = [
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+getEmployees();
