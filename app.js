997a2e73484063a5d4a672f54c689372270a2130
Wilmar A. Stephens <wilmars@alumni.brown.edu>
2020-10-18 00:29:08 -0400

    removed extra comments

--- app.js
+++ app.js
@@ -143,58 +143,6 @@ const getEmployee = async () => {
 
     }
 }
-// async function getEmployees() {
-//     await inquirer.prompt(questions)
-//         .then((answers) => {
-//             switch (answers.employeeRole) {
-//                 case "intern":
-//                     const intern = new Intern(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.internSchool)
-//                     responses.push(intern);
-//                     console.log(responses);
-
-//                     break;
-
-//                 case "engineer":
-//                     const engineer = new Engineer(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.engineerGithub)
-//                     responses.push(engineer);
-//                     console.log(responses);
-
-
-//                     break;
-//                 case "manager":
-//                     const manager = new Manager(answers.employeeName, answers.employeeId, answers.employeeEmail, answers.managerOffice)
-//                     responses.push(manager);
-//                     console.log(responses);
-
-//                     break;
-//             }
-//             if (answers.addAnother) {
-//                 getEmployees();
-//             } else {
-//                 return answers;
-//             }
-//         });
-
-// }
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
 async function main() {
     try {
